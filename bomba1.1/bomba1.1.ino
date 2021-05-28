fc93922d262a17b91224866e93f68e98b85a9199
Pol Domenech <pol.domenech.37@lacetania.cat>
2021-05-28 11:31:43 +0200

    implementant desactivacio

--- bomba1.1/bomba1.1.ino
+++ bomba1.1/bomba1.1.ino
@@ -8,6 +8,16 @@
 //********** Includes *************************************************************
 //********** Variables ************************************************************
 #include <Keypad.h> 
+#include <SPI.h>      // incluye libreria bus SPI
+#include <MFRC522.h>      // incluye libreria especifica para MFRC522
+#define RST_PIN  5      // constante para referenciar pin de reset
+#define SS_PIN  53      // constante para referenciar pin de slave select
+
+MFRC522 mfrc522(SS_PIN, RST_PIN); // crea objeto mfrc522 enviando pines de slave select y reset
+
+byte LecturaUID[4];         // crea array para almacenar el UID leido
+byte Usuario1[4]= {0x2A, 0xD6, 0x9F, 0x80} ;    // UID de tarjeta leido en programa 1
+byte Usuario2[4]= {0x09, 0xFF, 0x40, 0xE8} ;    // UID de llavero leido en programa 1
 byte teta;
 char TECLA;
 const byte FILAS=4;
@@ -20,7 +30,7 @@ char keys[FILAS][COLUMNAS]=
   {'*','0','#','D'}
 };
 byte pinesFilas[FILAS] = {9,8,7,6};   
-byte pinesColumnas[COLUMNAS] = {5,4,3,2}; 
+byte pinesColumnas[COLUMNAS] = {28,4,3,2}; 
 char PASS_RANDOM[7];
 char PASS[7];
 byte INDICE=0;
@@ -45,7 +55,9 @@ for(int i = 0; i < 6 ; i++)
   Serial.print(INDICE);
   }
   INDICE=0;
-  Serial.println("");  
+  Serial.println(""); 
+    SPI.begin();        // inicializa bus SPI
+  mfrc522.PCD_Init();     // inicializa modulo lector 
 }
 
 void loop() {
@@ -91,6 +103,7 @@ switch (ARMED)
 case 1:
       
       PLANTADA();
+      DESACTIVAR();
       break;
           
                          
@@ -123,7 +136,43 @@ void PLANTADA()
 
 void DESACTIVAR()
 {
+  if ( ! mfrc522.PICC_IsNewCardPresent())   // si no hay una tarjeta presente
+    return;           // retorna al loop esperando por una tarjeta
   
+  if ( ! mfrc522.PICC_ReadCardSerial())     // si no puede obtener datos de la tarjeta
+    return;           // retorna al loop esperando por otra tarjeta
+    
+    Serial.print("DESACTIVADA AMB:");       // muestra texto UID:
+    for (byte i = 0; i < mfrc522.uid.size; i++) { // bucle recorre de a un byte por vez el UID
+      if (mfrc522.uid.uidByte[i] < 0x10){   // si el byte leido es menor a 0x10
+        Serial.print(" 0");       // imprime espacio en blanco y numero cero
+        }
+        else{           // sino
+          Serial.print(" ");        // imprime un espacio en blanco
+          }
+          Serial.print(mfrc522.uid.uidByte[i], HEX);    // imprime el byte del UID leido en hexadecimal
+          LecturaUID[i]=mfrc522.uid.uidByte[i];     // almacena en array el byte del UID leido      
+          }
+          
+          Serial.print("\t");         // imprime un espacio de tabulacion             
+                    
+          if(comparaUID(LecturaUID, Usuario1))    // llama a funcion comparaUID con Usuario1
+            Serial.println("Bienvenido Usuario 1"); // si retorna verdadero muestra texto bienvenida
+          else if(comparaUID(LecturaUID, Usuario2)) // llama a funcion comparaUID con Usuario2
+            Serial.println("Bienvenido Usuario 2"); // si retorna verdadero muestra texto bienvenida
+           else           // si retorna falso
+            Serial.println("No te conozco");    // muestra texto equivalente a acceso denegado          
+                  
+                  mfrc522.PICC_HaltA();     // detiene comunicacion con tarjeta                
+}
+
+boolean comparaUID(byte lectura[],byte usuario[]) // funcion comparaUID
+{
+  for (byte i=0; i < mfrc522.uid.size; i++){    // bucle recorre de a un byte por vez el UID
+  if(lectura[i] != usuario[i])        // si byte de UID leido es distinto a usuario
+    return(false);          // retorna falso
+  }
+  return(true);           // si los 4 bytes coinciden retorna verdadero
 }
 
 
